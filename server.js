3d2bf31513f091e42d719bdc4930f5ade157ebf1
Varun Pattalachinti <vkp2@illinois.edu>
2020-07-09 21:21:20 -0500

    Making a POST route in node.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
 
-app.listen(8000,function()
-{
-  console.log("server is running")
-});
+
+
 
 const mockUserData = [
   {name: 'Mark'},
@@ -19,3 +19,31 @@ app.get('/users/:id', function(req,res){
     users: req.params.id
   })
 })
+
+app.post('/login',function(req,res)
+{
+  const username = req.body.username
+  const password = req.body.password
+
+  const mockUsername = "billyTheKid"
+  const mockPassword = "superSecret"
+
+  if(username === mockUsername && password == mockPassword)
+  {
+    res.json({
+      'success': true,
+      'message':'password and username',
+      'token': 'encrypted token goes here'
+    })
+  }
+  else {
+    res.json({
+      'success':false,
+      'message':'password and username goes here'
+    })
+  }
+})
+app.listen(8000,function()
+{
+  console.log("server is running")
+});
